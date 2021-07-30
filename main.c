8241533b388ddaf3ae3313c13f97b7efd125de76
Christopher Oosthuizen <christopher.jan.oosthuizen@gmail.com>
2021-07-30 12:36:29 -0500

    Added file functionality
    My text editor can edit and save files

--- main.c
+++ main.c
@@ -3,29 +3,66 @@
 #include "TextStorage.h"
 #include <stdio.h>
 
-int main() {
+void readFileContent(char* file_name, TextStorage* storage) {
+		FILE* file;
+		file = fopen(file_name, "r");
+		fseek(file, 0L, SEEK_END);
+		int file_size = ftell(file);
+		fseek(file, 0L, SEEK_SET);
+		char file_content[file_size];
+		fread(file_content, 1, file_size, file);
+		for(int i = 0; i < file_size; i++) {
+			appendTextStorage(storage,file_content[i]);
+		}
+		fclose(file);
+}
+
+void writeToFile(char* file_name, TextStorage* storage) {
+	FILE* file;
+	file = fopen(file_name, "w+");
+	DynamicString* content = getTextStorageText(storage); 
+	fprintf(file, content->str); 
+	freeDynamicString(content);
+	fclose(file);
+}
+
+void displayTextStorage(TextStorage* storage) {
+	clear();
+	DynamicString* display_str = getTextStorageText(storage);
+	addstr(display_str->str);
+	freeDynamicString(display_str);
+	refresh();
+	move(storage->y, storage->x);
+}
+
+int main(int argc, char* argv[]) {
 	TextStorage* str = createTextStorage();
+	if(argc > 1) {
+		readFileContent(argv[1], str);
+	}
 	initscr();
+	raw();
 	noecho();
 	clear();
 	cbreak();
 	keypad(stdscr, true);
 	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
-	refresh();
+	displayTextStorage(str);
 	while(TRUE) {
 		char c = getch();
-		if(c == 1) {
-			break;
-		}
+		endwin(); 
 
-		appendTextStorage(str,c); 
-		clear();
-		DynamicString* display_str = getTextStorageText(str);
-		addstr(display_str->str);
-		freeDynamicString(display_str);
-		refresh();
-		move(str->y, str->x);
-	}
+		if(c == 17) {
+			break;
+		} else if(c == 19) {
+			writeToFile(argv[1], str);	
+		} else {
+			appendTextStorage(str,c); 
+		}
+		displayTextStorage(str);
+
+
+		}
 	endwin();
 	freeTextStorage(str);
 	printf("success");
